73a1ab0f6c32b73026a98b01cd430bc47bf704b3
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-11-29 14:11:02 +0000

    Sorting process update

--- Activity.java
+++ Activity.java
@@ -3,19 +3,29 @@ public class Activity{
     public static void main(String args[]){
         int start[]={1,3,0,5,8,5};
         int end[]={2,4,6,7,9,9};
-        Arrays.sort(end);
+        //SORTING PART
+        //Arrays.sort(end);
+        int Activity[][]=new int[start.length][3];
+        for(int i=0;i<start.length;i++){
+            Activity[i][0]=i;
+            Activity[i][1]=start[i];
+            Activity[i][2]=end[i];
+
+        }
+        //LAMDA FUNCTION COMPARATORS
+        Arrays.sort(Activity,Comparator.comparingDouble(o->o[2]));
         int maxAct=0;
         ArrayList<Integer>ans=new ArrayList<>();
         //1st Activity
         maxAct=1;
-        ans.add(0);
-        int lastEnd=end[0];
+        ans.add(Activity[0][0]);
+        int lastEnd=Activity[0][2];
         for(int i=1;i<end.length;i++){
-            if(start[i]>=lastEnd){
+            if(Activity[i][1]>=lastEnd){
                 //Activity selected
                 maxAct++;
-                ans.add(i);
-                lastEnd=end[i];
+                ans.add(Activity[i][0]);
+                lastEnd=Activity[i][2];
             }
         }
         System.out.println("Max Activity="+maxAct);
