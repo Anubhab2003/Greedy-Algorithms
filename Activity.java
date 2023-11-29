f794d7a0e17540536387ed11082afd4c8c089a43
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-11-29 13:32:57 +0000

    Activity selected

---
+++ Activity.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Activity{
+    public static void main(String args[]){
+        int start[]={1,3,0,5,8,5};
+        int end[]={2,4,6,7,9,9};
+        Arrays.sort(end);
+        int maxAct=0;
+        ArrayList<Integer>ans=new ArrayList<>();
+        //1st Activity
+        maxAct=1;
+        ans.add(0);
+        int lastEnd=end[0];
+        for(int i=1;i<end.length;i++){
+            if(start[i]>=lastEnd){
+                //Activity selected
+                maxAct++;
+                ans.add(i);
+                lastEnd=end[i];
+            }
+        }
+        System.out.println("Max Activity="+maxAct);
+        for(int i=0;i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
